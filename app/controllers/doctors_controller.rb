3fd21837c56d10cf8e5a592da0cf1cd36d5160a5
github username <github email address>
2018-05-26 12:18:20 -0400

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,22 @@
+require 'date'
+class DoctorsController < ApplicationController
+  before_action :get_doctor, only: [:show]
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def get_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :department)
+  end
+
+end
